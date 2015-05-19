84447f56e1455c47cc36b94f749f2eb5ca9a9fb5
Rand01ph <361212419@qq.com>
2015-05-20 00:20:24 +0800

    add the 404function and change mime type for less

--- index.js
+++ index.js
@@ -6,19 +6,36 @@ var makeJade = require('./lib/processor/jade');
 var makeLess = require('./lib/processor/less');
 var path = require('path');
 
+
 //route
-function setRoute(req, res, next) {
-	if (req.url == '/') {
-		req.url += 'index.html';
+function setRoute() {
+	return function (req, res, next) {
+		if (req.url === '/') {
+			req.url += 'index.html';
+		}
+		else {
+			next();
+		}
 	}
-	else {
-		next();
-	}
+
 }
 
 
+//404
+function prevent() {
+	return function (req, res, next) {
+		if (path.extname(req.url) == '.jade' || path.extname(req.url) == '.less') {
+            res.statusCode = 404;
+            res.end();
+        }
+		else {
+			next();
+		}
+	}
+}
+
 function createMiniHarp(root) {
 	var app = connect();
-	app.use(setRoute).use(serveStatic(root)).use(makeJade(root)).use(makeLess(root));
+	app.use(setRoute()).use(prevent()).use(serveStatic(root)).use(makeJade(root)).use(makeLess(root));
 	return app;
 }
